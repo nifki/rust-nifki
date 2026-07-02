b8473f4802ae0d39f7d4a3b9e0084350fdccf8e8
Alistair Turnbull <github@minworks.co.uk>
2026-07-02 17:11:30 +0100

    Use the value of `NIFKI_BASE_URL` if it is set.

--- src/main.rs
+++ src/main.rs
@@ -265,10 +265,11 @@ fn main() {
     let wiki_root = env::var("NIFKI_WIKI_ROOT").unwrap_or_else(|_| WIKI_ROOT.to_owned());
     let js_root = env::var("NIFKI_JS_ROOT").unwrap_or_else(|_| JS_ROOT.to_owned());
     let static_root = env::var("NIFKI_STATIC_ROOT").unwrap_or_else(|_| STATIC_ROOT.to_owned());
+    let base_url = env::var("NIFKI_BASE_URL").ok();
     let server = Nifki {
         wiki_root: Path::new(wiki_root.as_str()).into(),
         js_root: Path::new(js_root.as_str()).into(),
         static_root: Path::new(static_root.as_str()).into(),
     };
-    ph::start("localhost:8080".into(), None, server);
+    ph::start("localhost:8080".into(), base_url, server);
 }
